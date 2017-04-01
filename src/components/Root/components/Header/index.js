5a1e5f35bab5c4e0a3f4448ae4a736c622b6dc30
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-04-01 03:28:09 -0400

    Add react-router and refactor for routing

--- src/components/Root/components/Header/index.js
+++ src/components/Root/components/Header/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const activeStyle = {
+  color: 'green',
+}
 
 const Header = () => (
-  <header>Header</header>
+  <nav>
+    <NavLink exact activeStyle={activeStyle} to="/">Home</NavLink>
+    <NavLink activeStyle={activeStyle} to="/about">About</NavLink>
+  </nav>
 )
 
 export default Header
